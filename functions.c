78d5cd3246b5b0aef8c1fa8c8c844969aaf66b20
flaviomco <2109@holbertonschool.com>
2020-10-27 23:22:51 +0000

    functions.c

--- functions.c
+++ functions.c
@@ -1,33 +1,43 @@
 #include "holberton.h"
 
 /**
- * print_c - prints a character
- * @args: arguments for _printf
- * Return: number of char printed
+ * print_c - prints a single char
+ * @args: arguments received from _printf
+ * Return: numbers of chars
  */
 int print_c(va_list args)
 {
 	char c = va_arg(args, int);
 
-	_putchar(c);
+	write(1, &c, 1);
 	return (1);
 }
+
+
 /**
- * print_s - loops through a string and prints every character
- * @args: arguments for _printf
- * Return: number of char printed
+ * print_s - prints a string
+ * @args: arguments received from _printf
+ * Return: number of chars
  */
 int print_s(va_list args)
 {
 	int i;
-	char *c = va_arg(args, char *);
+	char *str;
 
-	for (i = 0; c[i] != '\0'; i++)
+	str = va_arg(args, char *);
+	if (str == NULL)
 	{
-		_putchar(c[i]);
+		str = "(null)";
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		write(1, &str[i], 1);
 	}
 	return (i);
 }
+
+
+
 /**
  * print_i - print integers
  * @args: arguments for _printf
